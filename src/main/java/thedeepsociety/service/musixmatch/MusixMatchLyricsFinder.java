9b0fc9d68fa023e89e94d47fcee2f23e80012f18
Rustam Musin <PloadyFree@gmail.com>
2017-05-16 01:32:00 +0400

    Added manual caching to prevent situations when api server is unavailable

--- src/main/java/thedeepsociety/service/musixmatch/MusixMatchLyricsFinder.java
+++ src/main/java/thedeepsociety/service/musixmatch/MusixMatchLyricsFinder.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import thedeepsociety.service.Lyrics;
@@ -25,37 +24,56 @@ public class MusixMatchLyricsFinder implements LyricsFinder {
     private final Cache cache;
 
     @Autowired
-    public MusixMatchLyricsFinder(MusixMatch musixMatch, CacheManager cm) {
+    public MusixMatchLyricsFinder(MusixMatch musixMatch, @SuppressWarnings("SpringJavaAutowiringInspection") CacheManager cm) {
         this.musixMatch = musixMatch;
         cache = cm.getCache(cacheName);
     }
 
     @Override
-    @Cacheable(value = cacheName, key = "#artist + ' - ' + #track", unless = "#result == null")
     public Lyrics find(String artist, String track) {
         try {
+            Lyrics result = fromCache(artist, track);
+            if (result != null)
+                return result;
+
             Track matchingTrack = musixMatch.getMatchingTrack(track, artist);
             TrackData trackData = matchingTrack.getTrack();
             if (trackData.getHasLyrics() == 0) {
-                cache.put(artist + " - " + track, null);
+                cache(artist, track, null);
                 return null;
             }
 
             org.jmusixmatch.entity.lyrics.Lyrics lyrics = musixMatch.getLyrics(trackData.getTrackId());
-
             log.info(() -> "Successfully found for " + artist + " " + track);
 
-            return Lyrics.builder()
+            result = Lyrics.builder()
                     .artist(trackData.getArtistName())
                     .track(trackData.getTrackName())
                     .text(lyrics.getLyricsBody())
                     .source(getName())
                     .build();
+            cache(artist, track, result);
+            return result;
         } catch (MusixMatchException e) {
-            log.warning(() -> "Thrown exception on " + artist + " - " + track +": " + e);
-            e.printStackTrace();
+            log.throwing(MusixMatchLyricsFinder.class.toString(), "find(String, String)", e);
+            cache(artist, track, null);
             return null;
         }
+    }
+
+    private void cache(String artist, String track, Lyrics lyrics) {
+        cache.put(cacheKey(artist, track), lyrics);
+    }
+
+    private Lyrics fromCache(String artist, String track) {
+        Cache.ValueWrapper cached = cache.get(cacheKey(artist, track));
+        if (cached == null)
+            return null;
+        return (Lyrics) cached.get();
+    }
+
+    private String cacheKey(String artist, String track) {
+        return artist + " - " + track;
     }
 
     @Override
